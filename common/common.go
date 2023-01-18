3ca243d03a4bd6ac6bf4eca026d5971cbe3a9a75
Harsh.Trivedi <trivediharsh49@gmail.com>
2023-01-19 02:17:06 +0530

    Initialize fix-app-install-logic

---
+++ common/common.go
@@ -0,0 +1,52 @@
+package common
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/google/go-github/github"
+	"github.com/tcnksm/go-latest"
+)
+
+var (
+	// OutputFields for custom format output
+	OutputFields string
+	// OutputFormat for custom format output
+	OutputFormat string
+	// RegionSet picks the region to connect to, if you use this option it will use it over the default region
+	RegionSet string
+	// DefaultYes : automatic yes to prompts; assume \"yes\" as answer to all prompts and run non-interactively
+	DefaultYes bool
+	// PrettySet : Prints the json output in pretty format
+	PrettySet bool
+	// VersionCli is set from outside using ldflags
+	VersionCli = "0.0.0"
+	// CommitCli is set from outside using ldflags
+	CommitCli = "none"
+	// DateCli is set from outside using ldflags
+	DateCli = "unknown"
+)
+
+// VersionCheck checks if there is a new version of the CLI
+func VersionCheck() (res *latest.CheckResponse, skip bool) {
+	githubTag := &latest.GithubTag{
+		Owner:             "civo",
+		Repository:        "cli",
+		FixVersionStrFunc: latest.DeleteFrontV(),
+	}
+	res, err := latest.Check(githubTag, strings.Replace(VersionCli, "v", "", 1))
+	if err != nil {
+		if IsGHError(err) {
+			return nil, true
+		}
+		fmt.Printf("Checking for a newer version failed with %s \n", err)
+		return nil, true
+	}
+	return res, false
+}
+
+// IsGHError checks if the error is a github rate limit error
+func IsGHError(err error) bool {
+	_, ok := err.(*github.ErrorResponse)
+	return ok
+}
---
+++ main.go
@@ -0,0 +1,43 @@
+/*
+Copyright © 2019 Civo Ltd <hello@civo.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/civo/cli/cmd"
+	"github.com/civo/cli/common"
+	"github.com/savioxavier/termlink"
+)
+
+func main() {
+	defer func() {
+		if err := recover(); err != nil {
+			resp, skip := common.VersionCheck()
+			if skip == true {
+				fmt.Println("An error occurred fetching the CLI tag, please try again later")
+				os.Exit(1)
+			}
+			res := resp.Current
+
+			updateCmd := "civo update"
+			gitIssueLink := termlink.ColorLink("GitHub issue", "https://github.com/civo/cli/issues", "italic green")
+			fmt.Printf("panic : %s \nYour CLI Version : %s \nPlease, run %q and retry the command \nIf you are still facing issues, please report it on our community slack or open a %s \n", err, res, updateCmd, gitIssueLink)
+		}
+	}()
+	cmd.Execute()
+}
