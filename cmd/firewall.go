e0cc85e08ad51c29084e3ec4d3a9e67f4caa9603
Harsh.Trivedi <trivediharsh49@gmail.com>
2023-01-19 02:16:45 +0530

    Initialize feature/lb

---
+++ cmd/firewall.go
@@ -0,0 +1,58 @@
+package cmd
+
+import (
+	"errors"
+
+	"github.com/spf13/cobra"
+)
+
+var firewallCmd = &cobra.Command{
+	Use:     "firewall",
+	Aliases: []string{"firewalls", "fw"},
+	Short:   "Details of Civo firewalls",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := cmd.Help()
+		if err != nil {
+			return err
+		}
+		return errors.New("a valid subcommand is required")
+	},
+}
+
+var firewallRuleCmd = &cobra.Command{
+	Use:     "rule",
+	Aliases: []string{"rules"},
+	Short:   "Details of Civo firewalls rules",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := cmd.Help()
+		if err != nil {
+			return err
+		}
+		return errors.New("a valid subcommand is required")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(firewallCmd)
+	firewallCmd.AddCommand(firewallListCmd)
+	firewallCmd.AddCommand(firewallCreateCmd)
+	firewallCmd.AddCommand(firewallUpdateCmd)
+	firewallCmd.AddCommand(firewallRemoveCmd)
+
+	firewallCreateCmd.Flags().StringVarP(&firewallnetwork, "network", "n", "default", "the network to create the firewall")
+
+	// Firewalls rule cmd
+	firewallCmd.AddCommand(firewallRuleCmd)
+	firewallRuleCmd.AddCommand(firewallRuleListCmd)
+	firewallRuleCmd.AddCommand(firewallRuleCreateCmd)
+	firewallRuleCmd.AddCommand(firewallRuleRemoveCmd)
+
+	// Flags for firewall rule create cmd
+	firewallRuleCreateCmd.Flags().StringVarP(&protocol, "protocol", "p", "TCP", "the protocol choice (TCP, UDP, ICMP)")
+	firewallRuleCreateCmd.Flags().StringVarP(&startPort, "startport", "s", "", "the start port of the rule")
+	firewallRuleCreateCmd.Flags().StringVarP(&endPort, "endport", "e", "", "the end port of the rule")
+	firewallRuleCreateCmd.Flags().StringVarP(&cidr, "cidr", "c", "0.0.0.0/0", "the CIDR of the rule you can use (e.g. -c 10.10.10.1/32,148.2.6.120/32)")
+	firewallRuleCreateCmd.Flags().StringVarP(&direction, "direction", "d", "ingress", "the direction of the rule (only 'ingress' is supported now)")
+	firewallRuleCreateCmd.Flags().StringVarP(&label, "label", "l", "", "a string that will be the displayed as the name/reference for this rule")
+	firewallRuleCreateCmd.MarkFlagRequired("startport")
+}
---
+++ utility/confirmation.go
@@ -0,0 +1,74 @@
+package utility
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// retrieveUserInput is a function that can retrieve user input in form of string. By default,
+// it will prompt the user. In test, you can replace this with code that returns the appropriate response.
+var retrieveUserInput = func(message string) (string, error) {
+	return readUserInput(os.Stdin, message)
+}
+
+// readUserInput is a io.Reader to read user input from. It is meant to allow simplified testing
+// as to-be-read inputs can be injected conveniently.
+func readUserInput(in io.Reader, message string) (string, error) {
+	reader := bufio.NewReader(in)
+	YellowConfirm("Are you sure you want to " + message + " (y/N) ? ")
+	answer, err := reader.ReadString('\n')
+	if err != nil {
+		return "", err
+	}
+
+	answer = strings.TrimRight(answer, "\r\n")
+
+	return strings.ToLower(answer), nil
+}
+
+// AskForConfirm parses and verifies user input for confirmation.
+func AskForConfirm(message string) error {
+	answer, err := retrieveUserInput(message)
+	if err != nil {
+		Error("Unable to parse users input: %s", err)
+	}
+
+	if answer != "y" && answer != "ye" && answer != "yes" {
+		return fmt.Errorf("invalid user input")
+	}
+
+	return nil
+}
+
+// UserConfirmedDeletion builds a message to ask the user to confirm delete
+// a resource and then sends it through to AskForConfirm to
+// parses and verifies user input.
+func UserConfirmedDeletion(resourceType string, ignoringConfirmed bool, objectToDelete string) bool {
+	if !ignoringConfirmed {
+		message := fmt.Sprintf("delete the %s %s", Green(objectToDelete), resourceType)
+		err := AskForConfirm(message)
+		if err != nil {
+			return false
+		}
+	}
+
+	return true
+}
+
+// UserConfirmedOverwrite builds a message to ask the user to confirm overwrite config
+// and then sends it through to AskForConfirm to
+// parses and verifies user input.
+func UserConfirmedOverwrite(resourceType string, ignoringConfirmed bool) bool {
+	if !ignoringConfirmed {
+		message := fmt.Sprintf("overwrite the %s", resourceType)
+		err := AskForConfirm(message)
+		if err != nil {
+			return false
+		}
+	}
+
+	return true
+}
