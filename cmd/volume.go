0bf2ed41c0163ab4664cb9ac507875210eb54e03
Harsh.Trivedi <trivediharsh49@gmail.com>
2023-01-19 02:16:30 +0530

    Initialize exclude-k3s-disk-images

---
+++ cmd/volume.go
@@ -0,0 +1,42 @@
+package cmd
+
+import (
+	"errors"
+
+	"github.com/spf13/cobra"
+)
+
+var volumeCmd = &cobra.Command{
+	Use:     "volume",
+	Aliases: []string{"volumes"},
+	Short:   "Details of Civo volumes",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		err := cmd.Help()
+		if err != nil {
+			return err
+		}
+		return errors.New("a valid subcommand is required")
+	},
+}
+
+func init() {
+	// rootCmd.AddCommand(volumeCmd)
+	volumeCmd.AddCommand(volumeListCmd)
+	volumeCmd.AddCommand(volumeCreateCmd)
+	volumeCmd.AddCommand(volumeResizeCmd)
+	volumeCmd.AddCommand(volumeRemoveCmd)
+	volumeCmd.AddCommand(volumeAttachCmd)
+	volumeCmd.AddCommand(volumeDetachCmd)
+
+	volumeCreateCmd.Flags().BoolVarP(&bootableVolume, "bootable", "b", false, "Mark the volume as bootable")
+	volumeCreateCmd.Flags().IntVarP(&createSizeGB, "size-gb", "s", 0, "The new size in GB (required)")
+	volumeCreateCmd.Flags().StringVarP(&networkVolumeID, "network", "t", "default", "The network where the volume will be created")
+	volumeCreateCmd.MarkFlagRequired("size-gb")
+
+	volumeResizeCmd.Flags().IntVarP(&newSizeGB, "size-gb", "s", 0, "The new size in GB (required)")
+	volumeResizeCmd.MarkFlagRequired("size-gb")
+
+	volumeAttachCmd.Flags().BoolVarP(&waitVolumeAttach, "wait", "w", false, "wait until the volume is attached")
+
+	volumeDetachCmd.Flags().BoolVarP(&waitVolumeDetach, "wait", "w", false, "wait until the volume is detached")
+}
