8d45029c3afe158f2ab6e6d98a07072d8537166b
Harsh.Trivedi <trivediharsh49@gmail.com>
2023-01-19 02:16:37 +0530

    Initialize issue-142

---
+++ cmd/domain_list.go
@@ -0,0 +1,53 @@
+package cmd
+
+import (
+	"github.com/civo/cli/config"
+	"github.com/civo/cli/utility"
+
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var domainListCmd = &cobra.Command{
+	Use:     "ls",
+	Aliases: []string{"list", "all"},
+	Short:   "List domains",
+	Long: `List all current domains.
+If you wish to use a custom format, the available fields are:
+
+	* id
+	* name
+
+Example: civo domain ls -o custom -f "ID: Name"`,
+	Run: func(cmd *cobra.Command, args []string) {
+		client, err := config.CivoAPIClient()
+		if err != nil {
+			utility.Error("Creating the connection to Civo's API failed with %s", err)
+			os.Exit(1)
+		}
+
+		domains, err := client.ListDNSDomains()
+		if err != nil {
+			utility.Error("%s", err)
+			os.Exit(1)
+		}
+
+		ow := utility.NewOutputWriter()
+		for _, domain := range domains {
+			ow.StartLine()
+
+			ow.AppendDataWithLabel("id", domain.ID, "ID")
+			ow.AppendDataWithLabel("name", domain.Name, "Name")
+		}
+
+		switch outputFormat {
+		case "json":
+			ow.WriteMultipleObjectsJSON(prettySet)
+		case "custom":
+			ow.WriteCustomOutput(outputFields)
+		default:
+			ow.WriteTable()
+		}
+	},
+}
